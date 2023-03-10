c35ae171cc5f74004c590b1666ba21d900545337
Dionne Teh Wooi Ying <107110983+dionnieteh@users.noreply.github.com>
2023-03-11 04:56:58 +0800

    draft frontend payment info page
    1. draft of payment page
    2. display basic structure of payment page

---
+++ frontend/paymentPage.front.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<div class="payment">
+    <div class="container">
+        <h1>Payment Details</h1>
+        <div class="section">
+            <table>
+                <tr>
+                    <td>Package Name:</td>
+                    <td>fetch from package class</td>
+                </tr>
+                <tr>
+                    <td>Check-In Date:</td>
+                    <td>fetch from reservation class</td>
+                </tr>
+                <tr>
+                    <td>Check-Out Date:</td>
+                    <td>fetch from reservation class</td>
+                </tr>
+                <tr>
+                    <td>Package Price:</td>
+                    <td>unitPrice</td>
+                </tr>
+                <tr>
+                    <td>Total Price:</td>
+                    <td>unitPrice * unitsOfRoom</td>
+                </tr>
+                <tr>
+                    <td>Number of Package(s):</td>
+                    <td>unitsOfRoom</td>
+                </tr>
+                <tr>
+                    <td>Booking Fee: </td>
+                    <td>BOOKINGFEE</td>
+                </tr>
+                <tr>
+                    <td>Total Amount Payable:</td>
+                    <td>finalPrice</td>
+                </tr>
+            </table>
+        </div>
+
+</html>
