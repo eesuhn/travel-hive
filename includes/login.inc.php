d01fe8ab6ebedc20badd995fae60de8310c21f04
eesuhn <102596628+eesuhn@users.noreply.github.com>
2023-03-13 14:40:22 +0800

    Account login navigation
    redirect to:
    - home page if customer login
    - dashboard if hotel login

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -1,24 +1,33 @@
 <?php
+    if (isset($_POST["submit"])) {
 
-if (isset($_POST["submit"])){
+        //Grabbing data
+        $email = $_POST["email"];
+        $pwd = $_POST["pwd"];
 
-    //Grabbing data
-    $email = $_POST["email"];
-    $pwd = $_POST["pwd"];
+        //Instantiate what is needed
+        include "../backend/connection.back.php";
+        include "../backend/account.back.php";
 
-    //Instantiate what is needed
-    include "../backend/connection.back.php";
-    include "../backend/account.back.php";
+        //Function to check user
+        $login = new Account();
+        $login->setLoginDetails($email,$pwd);
 
-    //Function to check user
-    $login = new Account();
-    $login->setLoginDetails($email,$pwd);
+        if ($_POST["accType"]==="customer") {
+            $login->loginUser();
+        } else {
+            $login->loginHotel();
+        }
 
-    if ($_POST["accType"]==="customer"){
-    $login->loginUser();
-    }else{
-    $login->loginHotel();
+        /* 
+            redirect to 
+                home page if customer login
+                dashboard if hotel login
+        */
+        if ($_POST["accType"]==="customer") {
+            header("Location: ../index/index.php");
+        } else {
+            header("Location: ../frontend/dashboard_hotel.front.php");
+        }
     }
-
-}
 ?>
