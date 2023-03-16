15ec5662fbc0152417f6990052c316481f7b1147
eesuhn <102596628+eesuhn@users.noreply.github.com>
2023-03-16 20:44:52 +0800

    Hotel to add rooms to package
    1. Created room table
    2. room.back for registering new room

--- backend/room.back.php
+++ backend/room.back.php
@@ -1,34 +1,40 @@
 <?php
     class Room extends Packages{
 
-        private $roomId;
         private $roomNum;
         private $packageId;
         private $hotelUid;
 
-        public function __construct(){
-            $this->roomId = "";
+        // constructor with empty parameters
+        public function __construct() {
             $this->roomNum = "";
             $this->packageId = "";
             $this->hotelUid = "";
         }
 
-        public function setRoomDetails($roomId,$roomNum,$packageId,$hotelUid){
-            $this->roomId = $roomId;
+        // setter for every attributes
+        public function setRoomDetails($roomNum, $packageId, $hotelUid) {
             $this->roomNum = $roomNum;
             $this->packageId = $packageId;
             $this->hotelUid = $hotelUid;
         }
 
-        public function getRoomId ($packageId){
-            $sql = "SELECT roomId FROM room where packageId = '$packageId';";
-            
-            $stmt = $this->connect()->prepare($sql);
-            $stmt->execute();
+        // create new room in database
+        public function registerRoom () {
+            $sql = "INSERT INTO room (roomNum, packageId, hotelUid) VALUES (:value1, :value2, :value3)";
 
-            if ($stmt->rowCount() > 0) {
-                $result = $stmt->fetch(PDO::FETCH_ASSOC);
-                return $result['roomId'];
+            $stmt = $this->connect()->prepare($sql);
+
+            $stmt->bindParam(':value1', $value1);
+            $stmt->bindParam(':value2', $value2);
+            $stmt->bindParam(':value3', $value3);
+
+            $value1 = $this->roomNum;
+            $value2 = $this->packageId;
+            $value3 = $this->hotelUid;
+
+            if ($stmt->execute(array(':value1' => $value1, ':value2' => $value2, ':value3' => $value3))) {
+                echo "<script>alert('Room successfully created'); window.location.href='../frontend/packagesH.front.php'</script>";
             } else {
                 $error = $stmt->errorInfo();
                 echo "Error: " . $error[2];
@@ -36,9 +42,8 @@
         }
 
         // get all package id based on hotel id, put it in an array
-        /*
-        public function getRoomId ($hotelUid) {
-            $sql = "SELECT packageID FROM packages where hotelUid = '$hotelUid';";
+        public function getRoomNum ($packageId) {
+            $sql = "SELECT roomNum FROM room where packageId = '$packageId';";
 
             $stmt = $this->connect()->prepare($sql);
             $stmt->execute();
@@ -52,28 +57,5 @@
                 return "Error: " . $error[2];
             }
         }
-
-        public function showRoom ($packageID) {
-            $sql = "SELECT * FROM room where packageID = '$packageID';";
-            
-            $stmt = $this->getConnection()->prepare($sql);
-            $stmt->execute();
-
-            if ($stmt->rowCount() > 0) {
-                $result = $stmt->fetch(PDO::FETCH_ASSOC);
-                return $result['roomNum'];
-            } else {
-                $error = $stmt->errorInfo();
-                echo "Error: " . $error[2];
-            }
-        }
-        */
-
-
-
-        
-
-
-
     }
 ?>
--- frontend/packagesH.front.php
+++ frontend/packagesH.front.php
@@ -29,19 +29,19 @@
                 <div class="btn-group mr-1 pull-right">
                     <a href="#addRoom" data-bs-toggle="modal" data-bs-target="#addRoom<?=$id?>" class="btn btn-primary btn-sm">Add rooms</a>
                 </div>
-                <h5 class="card-title"><?=$packages->showName($id);?></h5>
+                    <h5 class="card-title"><?=$packages->showName($id);?></h5>
                     <p class="card-text"><small>RM <?=$packages->showPrice($id);?></small></p>
                     <p class="card-text"><small class="text-muted">
                         <?=$packages->showDesc($id);?>
                     </small></p>
                     <div class="btn-group mr-1">
-                    <a href="#updatePackage" data-bs-toggle="modal" data-bs-target="#updatePackage<?=$id?>" class="btn btn-primary btn-sm">Update</a>
+                        <a href="#updatePackage" data-bs-toggle="modal" data-bs-target="#updatePackage<?=$id?>" class="btn btn-primary btn-sm">Update</a>
                     </div>
                     <div class="btn-group mr-1">
-                    <form action="../includes/packagesH.inc.php" method="POST">
-                        <input type="hidden" name="packageID" value="<?=$id?>">
-                        <button type="submit" name="delete" class="btn btn-danger btn-sm">Delete</button>
-                    </form>
+                        <form action="../includes/packagesH.inc.php" method="POST">
+                            <input type="hidden" name="packageID" value="<?=$id?>">
+                            <button type="submit" name="delete" class="btn btn-danger btn-sm">Delete</button>
+                        </form>
                     </div>
             </div>
         </div>
@@ -57,15 +57,27 @@
                 <div class="modal-body">
                     <div class="row d-flex justify-content-center">
                         <div class="col-lg-12">
-                        <form id="add_rooms" method="POST" action="../includes/room.inc.php" enctype="multipart/form-data">
+                        <form id="add_rooms" method="POST" action="../includes/packagesH.inc.php" enctype="multipart/form-data">
                             <div class="form-row">
-                                Rooms Available: 
+
+                                <div class="mb-3">
+                                    <label for="">Room Available: </label>
+                                    <textarea class="form-control" id="" name="" rows="5" style="resize: none;" readonly><?php
+                                    $roomNum = $room->getRoomNum($id);
+                                    if (is_array($roomNum)) {
+                                        foreach ($roomNum as $row) {
+                                            echo $row['roomNum'];
+                                            echo "\n";
+                                        }
+                                    }?></textarea>
+                                </div>
 
                                 <input type="hidden" name="packageID" value="<?=$id?>">
                                 <div class="mb-3">
                                     <label for="">Room Number</label>
-                                    <input type="text" class="form-control" id="" name="" value="">
+                                    <input type="text" class="form-control" id="roomNum" name="roomNum" value="">
                                 </div>
+
                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                                 <button class="btn btn-primary" name="submitR" id="submitR">Save changes</button>
                             </form>
